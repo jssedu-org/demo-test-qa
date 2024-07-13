242998ad33eaa9425d9db561155f92d754baf9b0
Jesús Salvador Sánchez Córdoba <jesussalvador.sanchez@edu.upct.es>
2024-07-14 00:05:54 +0200

    Initial commit

---
+++ module-boot/src/main/java/com/usdev/demotestqa/DemoTestQaApplication.java
@@ -0,0 +1,13 @@
+package com.usdev.demotestqa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTestQaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTestQaApplication.class, args);
+    }
+
+}
---
+++ module-boot/src/test/java/com/usdev/demotestqa/DemoTestQaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usdev.demotestqa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTestQaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
