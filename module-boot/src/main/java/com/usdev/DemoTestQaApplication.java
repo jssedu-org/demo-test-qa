0fdd3511cb6164103bd0088c9af972b1cb046089
Jesús Salvador Sánchez Córdoba <jesussalvador.sanchez@edu.upct.es>
2024-12-25 14:41:27 +0100

    feat(XXX): test commit

--- module-boot/src/main/java/com/usdev/DemoTestQaApplication.java
+++ module-boot/src/main/java/com/usdev/DemoTestQaApplication.java
@@ -3,7 +3,7 @@ package com.usdev.demotestqa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.usdev"})
 public class DemoTestQaApplication {
 
     public static void main(String[] args) {
